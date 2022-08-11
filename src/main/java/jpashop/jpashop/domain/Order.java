9fd7cddad781c7c5b3b7e68a37ffee96af0f20d9
jhsong2580 <thdwmdgns@naver.com>
2022-08-11 16:22:41 +0900

    feat: Order Entity 생성

---
+++ src/main/java/jpashop/jpashop/domain/Order.java
@@ -0,0 +1,41 @@
+package jpashop.jpashop.domain;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.TableGenerator;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "ORDERS")
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@TableGenerator(
+    name = "ORDER_SEQ",
+    table = "ORDER_SEQ_GEN",
+    initialValue = 1,
+    allocationSize = 50
+)
+@AttributeOverrides({
+    @AttributeOverride(name = "createDate", column = @Column(name = "ORDER_DATE")),
+    @AttributeOverride(name = "updateDate", column = @Column(name = "ORDER_UPDATE_DATE"))
+})
+public class Order extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "ORDER_SEQ_GEN")
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+}
