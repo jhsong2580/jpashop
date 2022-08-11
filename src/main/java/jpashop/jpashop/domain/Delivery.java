e6c09d4caee613ef54eed537b671ce556c6f13b0
jhsong2580 <thdwmdgns@naver.com>
2022-08-11 16:11:15 +0900

    feat: Delivery Entity 생성

---
+++ src/main/java/jpashop/jpashop/domain/Delivery.java
@@ -0,0 +1,36 @@
+package jpashop.jpashop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@SequenceGenerator(name = "DELIVERY_SEQ_GEN", //sequence generator 이름
+    sequenceName = "DELIVERY_SEQ", //sequence 이름
+    initialValue = 1, // 시작값
+    allocationSize = 1) //메모리에 한번에 로딩할 sequence 갯수
+public class Delivery {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "DELIVERY_SEQ_GEN")
+    @Column(name = "DELIVERY_ID")
+    private Long id;
+
+    @Embedded
+    private Address address = new Address();
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "STATUS")
+    private DeliveryStatus deliveryStatus;
+}
