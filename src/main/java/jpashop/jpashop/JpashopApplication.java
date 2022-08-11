00b292020b6bcbc08689a82f9de086d2f6636bd9
jhsong2580 <thdwmdgns@naver.com>
2022-08-11 15:56:18 +0900

    feat: Auditing 기능 활성화

--- src/main/java/jpashop/jpashop/JpashopApplication.java
+++ src/main/java/jpashop/jpashop/JpashopApplication.java
@@ -2,12 +2,14 @@ package jpashop.jpashop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class JpashopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JpashopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JpashopApplication.class, args);
+    }
 
 }
