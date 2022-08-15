838a36998f947bdf2e7a9564b95d81a555a83300
jhsong2580 <thdwmdgns@naver.com>
2022-08-15 23:58:30 +0900

    feat: login / join 시 BindingError 존재시 재입력 기능 구현

--- src/main/java/jpashop/jpashop/controller/MemberController.java
+++ src/main/java/jpashop/jpashop/controller/MemberController.java
@@ -1,7 +1,13 @@
 package jpashop.jpashop.controller;
 
+import jpashop.jpashop.dto.member.request.MemberJoinDTO;
+import jpashop.jpashop.dto.member.request.MemberLoginDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,12 +15,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MemberController {
 
     @GetMapping("/login")
-    public String loginPage (){
+    public String loginPage(@ModelAttribute MemberLoginDTO memberLoginDTO) {
         return "/basic/login";
     }
-    
+
+    @PostMapping("/login")
+    public String loginProcess(@Validated @ModelAttribute MemberLoginDTO loginDTO,
+        BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "/basic/login";
+        }
+        return "redirect:/member/login";
+    }
+
     @GetMapping("/join")
-    public String joinPage(){
+    public String joinPage(@ModelAttribute MemberJoinDTO memberJoinDTO) {
         return "/basic/join";
+    }
+
+    @PostMapping("/join")
+    public String joinProcess(@Validated @ModelAttribute MemberJoinDTO memberJoinDTO,
+        BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "/basic/join";
+        }
+        return "redirect:/member/login";
     }
 }
