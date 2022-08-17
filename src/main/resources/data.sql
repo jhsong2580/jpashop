2e9c9f32a3bf04273a64d3ee0900621f40fd4fba
jhsong2580 <thdwmdgns@naver.com>
2022-08-17 15:12:42 +0900

    item 자동 삽입 sql 생성

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,22 @@
 insert into member(member_id, member_create_date, member_update_date, city, street, zip_code, name,
                    password)
 values (default, now(), now(), 'seoul', 'kangnam', '06284', '10149424', '10149424');
+
+insert into item values('M',default, '드래곤길들이기', 18000, 10);
+insert into movie values('드래곤1','드래곤2',1);
+
+insert into item values('M', default, '씨비스트', 16000, 4);
+insert into movie values('드래곤1','드래곤2',2);
+
+insert into item values('B', default, '책1', 28000, 10);
+insert into book values('저자1','18982',3);
+
+insert into item values('B', default, '책2', 38000, 4);
+insert into book values('저자2','28982',4);
+
+insert into item values('A', default, '앨범1', 58000, 10);
+insert into album values('아티스트1','etc1',5);
+
+
+insert into item values('A', default, '앨범2', 58000, 10);
+insert into album values('아티스트2','etc2',6);
