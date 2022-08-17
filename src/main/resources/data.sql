33b7e36d7ff90bc5e85a2ddb272be15cd7afbae8
jhsong2580 <thdwmdgns@naver.com>
2022-08-17 14:55:11 +0900

    사용자 자동 삽입 sql 생성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into member(member_id, member_create_date, member_update_date, city, street, zip_code, name,
+                   password)
+values (default, now(), now(), 'seoul', 'kangnam', '06284', '10149424', '10149424');
