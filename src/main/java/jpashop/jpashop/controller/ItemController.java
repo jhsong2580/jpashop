4ad28a317ae33d8df5caf4dacc6e906dc6aaad8c
jhsong2580 <thdwmdgns@naver.com>
2022-08-21 00:22:10 +0900

    feat: 업데이트 로직 페이지 연동 및 비즈니스 로직 연결

--- src/main/java/jpashop/jpashop/controller/ItemController.java
+++ src/main/java/jpashop/jpashop/controller/ItemController.java
@@ -2,6 +2,7 @@ package jpashop.jpashop.controller;
 
 import jpashop.jpashop.domain.ItemType;
 import jpashop.jpashop.dto.item.form.ItemAddDTO;
+import jpashop.jpashop.dto.item.form.ItemEditDTO;
 import jpashop.jpashop.dto.item.form.ItemValidationGroups;
 import jpashop.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
@@ -11,7 +12,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,6 +34,66 @@ public class ItemController {
         model.addAttribute("itemDTO", itemService.findById(itemId));
 
         return "basic/item";
+    }
+
+    @GetMapping("{id}/edit")
+    public String editItem(@PathVariable(name = "id") Long itemId, Model model) {
+        model.addAttribute("itemDTO", itemService.findByIdForEdit(itemId));
+        return "basic/editForm";
+    }
+
+    @PostMapping("/book/{id}/edit")
+    public String editBookItem(@PathVariable(name = "id") Long itemId,
+        @Validated({ItemValidationGroups.defaultGroup.class,
+            ItemValidationGroups.bookGroup.class}) @ModelAttribute ItemEditDTO itemEditDTO,
+        BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "basic/editForm";
+        }
+
+        itemService.edit(itemEditDTO, itemId);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("redirect:/items/");
+        stringBuilder.append(itemId);
+
+        return stringBuilder.toString();
+    }
+
+    @PostMapping("/movie/{id}/edit")
+    public String editMovieItem(@PathVariable(name = "id") Long itemId,
+        @Validated({ItemValidationGroups.defaultGroup.class,
+            ItemValidationGroups.movieGroup.class}) @ModelAttribute ItemEditDTO itemEditDTO,
+        BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "basic/editForm";
+        }
+
+        itemService.edit(itemEditDTO, itemId);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("redirect:/items/");
+        stringBuilder.append(itemId);
+
+        return stringBuilder.toString();
+    }
+
+    @PostMapping("/album/{id}/edit")
+    public String editAlbumItem(@PathVariable(name = "id") Long itemId,
+        @Validated({ItemValidationGroups.defaultGroup.class,
+            ItemValidationGroups.albumGroup.class}) @ModelAttribute ItemEditDTO itemEditDTO,
+        BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "basic/editForm";
+        }
+
+        itemService.edit(itemEditDTO, itemId);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("redirect:/items/");
+        stringBuilder.append(itemId);
+
+        return stringBuilder.toString();
     }
 
     @GetMapping("/add")
@@ -81,9 +141,5 @@ public class ItemController {
         return "redirect:/items";
     }
 
-    @PatchMapping
-    public String modifyItem() {
-        return "redirect:/items";
-    }
 
 }
