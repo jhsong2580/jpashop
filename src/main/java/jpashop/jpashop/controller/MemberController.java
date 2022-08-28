cfd5822624d9510c41a1eb2784e7bf7289b30fc1
jhsong2580 <thdwmdgns@naver.com>
2022-08-28 22:38:11 +0900

    fix: Member 로직에 대한 Controller -> RestController처리

--- src/main/java/jpashop/jpashop/controller/MemberController.java
+++ src/main/java/jpashop/jpashop/controller/MemberController.java
@@ -1,57 +1,48 @@
 package jpashop.jpashop.controller;
 
-import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 import jpashop.jpashop.dto.member.form.MemberJoinDTO;
 import jpashop.jpashop.dto.member.form.MemberLoginDTO;
 import jpashop.jpashop.service.MemberService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping(value = "/member")
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
 
-    @GetMapping("/login")
-    public String loginPage(@ModelAttribute MemberLoginDTO memberLoginDTO) {
-        return "/basic/login";
-    }
-
-    @PostMapping("/login")
-    public String loginProcess(@Validated @ModelAttribute MemberLoginDTO loginDTO,
-        BindingResult bindingResult,
-        HttpServletRequest httpServletRequest) {
+    @PostMapping(value = "/login", consumes = "application/json")
+    public ResponseEntity<String> loginProcess(@Validated @RequestBody MemberLoginDTO loginDTO,
+        BindingResult bindingResult) {
 
         if (bindingResult.hasErrors()) {
-            return "/basic/login";
+            throw new IllegalArgumentException(bindingResult.getFieldErrors().toString());
         }
 
-        memberService.login(loginDTO, httpServletRequest);
-        return "redirect:/items";
+        memberService.login(loginDTO);
+        // TODO: 2022/08/28 jwp Token
+        return ResponseEntity.ok(UUID.randomUUID().toString());
     }
 
-    @GetMapping("/join")
-    public String joinPage(@ModelAttribute MemberJoinDTO memberJoinDTO) {
-        return "/basic/join";
-    }
-
-    @PostMapping("/join")
-    public String joinProcess(@Validated @ModelAttribute MemberJoinDTO memberJoinDTO,
+    @PostMapping(consumes = "application/json")
+    public ResponseEntity<String> joinProcess(@Validated @RequestBody MemberJoinDTO memberJoinDTO,
         BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
-            return "/basic/join";
+            return ResponseEntity.badRequest().body(bindingResult.getFieldErrors().toString());
         }
 
         memberService.joinMember(memberJoinDTO);
 
-        return "redirect:/member/login";
+        return ResponseEntity.ok().build();
     }
+
 }
