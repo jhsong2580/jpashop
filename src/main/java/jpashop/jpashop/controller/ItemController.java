41786eef78fac1e69bb75b5ee1ccf133c994721b
jhsong2580 <thdwmdgns@naver.com>
2022-08-28 23:05:06 +0900

    fix: item 로직 RestController로 변경

--- src/main/java/jpashop/jpashop/controller/ItemController.java
+++ src/main/java/jpashop/jpashop/controller/ItemController.java
@@ -1,145 +1,54 @@
 package jpashop.jpashop.controller;
 
-import jpashop.jpashop.domain.ItemType;
+import jpashop.jpashop.dto.item.ItemDTO;
 import jpashop.jpashop.dto.item.form.ItemAddDTO;
 import jpashop.jpashop.dto.item.form.ItemEditDTO;
 import jpashop.jpashop.dto.item.form.ItemValidationGroups;
 import jpashop.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping(value = "/items")
 @RequiredArgsConstructor
 public class ItemController {
 
     private final ItemService itemService;
 
-    @GetMapping
-    public String getItems(Model model) {
-        model.addAttribute("items", itemService.getList());
-        return "basic/items";
+    @GetMapping(value = "{id}", consumes = "application/json")
+    public ResponseEntity<ItemDTO> getItemDetail(@PathVariable(name = "id") Long itemId) {
+        return ResponseEntity.ok(itemService.findById(itemId));
     }
 
-    @GetMapping("{id}")
-    public String getItemDetail(@PathVariable(name = "id") Long itemId, Model model) {
-        model.addAttribute("itemDTO", itemService.findById(itemId));
-
-        return "basic/item";
-    }
-
-    @GetMapping("{id}/edit")
-    public String editItem(@PathVariable(name = "id") Long itemId, Model model) {
-        model.addAttribute("itemEditDTO", itemService.findByIdForEdit(itemId));
-        return "basic/editForm";
-    }
-
-    @PostMapping("/book/{id}/edit")
-    public String editBookItem(@PathVariable(name = "id") Long itemId,
-        @Validated({ItemValidationGroups.defaultGroup.class,
-            ItemValidationGroups.bookGroup.class}) @ModelAttribute ItemEditDTO itemEditDTO,
+    @PatchMapping(value = "{id}", consumes = "application/json")
+    public ResponseEntity<ItemDTO> editBookItem(@PathVariable(name = "id") Long itemId,
+        @Validated({ItemValidationGroups.defaultGroup.class}) @RequestBody ItemEditDTO itemEditDTO,
         BindingResult bindingResult) {
+
         if (bindingResult.hasErrors()) {
-            return "basic/editForm";
+            throw new IllegalArgumentException(bindingResult.getFieldErrors().toString());
         }
 
-        itemService.edit(itemEditDTO, itemId);
-
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("redirect:/items/");
-        stringBuilder.append(itemId);
-
-        return stringBuilder.toString();
+        return ResponseEntity.ok().body(itemService.edit(itemEditDTO, itemId));
     }
 
-    @PostMapping("/movie/{id}/edit")
-    public String editMovieItem(@PathVariable(name = "id") Long itemId,
-        @Validated({ItemValidationGroups.defaultGroup.class,
-            ItemValidationGroups.movieGroup.class}) @ModelAttribute ItemEditDTO itemEditDTO,
+    @PostMapping(consumes = "application/json")
+    public ResponseEntity<ItemDTO> save(
+        @Validated(ItemValidationGroups.defaultGroup.class) @RequestBody ItemAddDTO itemAddDTO,
         BindingResult bindingResult) {
+
         if (bindingResult.hasErrors()) {
-            return "basic/editForm";
+            throw new IllegalArgumentException(bindingResult.getFieldErrors().toString());
         }
-
-        itemService.edit(itemEditDTO, itemId);
-
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("redirect:/items/");
-        stringBuilder.append(itemId);
-
-        return stringBuilder.toString();
+        return ResponseEntity.ok(itemService.save(itemAddDTO));
     }
-
-    @PostMapping("/album/{id}/edit")
-    public String editAlbumItem(@PathVariable(name = "id") Long itemId,
-        @Validated({ItemValidationGroups.defaultGroup.class,
-            ItemValidationGroups.albumGroup.class}) @ModelAttribute ItemEditDTO itemEditDTO,
-        BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return "basic/editForm";
-        }
-
-        itemService.edit(itemEditDTO, itemId);
-
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("redirect:/items/");
-        stringBuilder.append(itemId);
-
-        return stringBuilder.toString();
-    }
-
-    @GetMapping("/add")
-    public String addItem(@ModelAttribute ItemAddDTO itemAddDTO, Model model) {
-        model.addAttribute("itemTypes", ItemType.getItemTypeNamesForPage());
-        return "basic/addForm";
-    }
-
-    @PostMapping("/album")
-    public String saveAlbumItem(
-        @Validated(ItemValidationGroups.albumGroup.class) @ModelAttribute ItemAddDTO itemAddDTO,
-        BindingResult bindingResult, Model model) {
-
-        model.addAttribute("itemTypes", ItemType.getItemTypeNamesForPage());
-        if (bindingResult.hasErrors()) {
-            return "basic/addForm";
-        }
-        itemService.save(itemAddDTO);
-        return "redirect:/items";
-    }
-
-    @PostMapping("/book")
-    public String saveBookItem(
-        @Validated(ItemValidationGroups.bookGroup.class) @ModelAttribute ItemAddDTO itemAddDTO,
-        BindingResult bindingResult, Model model) {
-
-        model.addAttribute("itemTypes", ItemType.getItemTypeNamesForPage());
-        if (bindingResult.hasErrors()) {
-            return "basic/addForm";
-        }
-        itemService.save(itemAddDTO);
-        return "redirect:/items";
-    }
-
-    @PostMapping("/movie")
-    public String saveMovieItem(
-        @Validated(ItemValidationGroups.movieGroup.class) @ModelAttribute ItemAddDTO itemAddDTO,
-        BindingResult bindingResult, Model model) {
-
-        model.addAttribute("itemTypes", ItemType.getItemTypeNamesForPage());
-        if (bindingResult.hasErrors()) {
-            return "basic/addForm";
-        }
-        itemService.save(itemAddDTO);
-        return "redirect:/items";
-    }
-
-
 }
