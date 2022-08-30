8ab3ad7c587d68bbfb9d7eb67360e39bc0a592e4
jhsong2580 <thdwmdgns@naver.com>
2022-08-30 13:54:25 +0900

    fix: advice 적용 대상 변경

--- src/main/java/jpashop/jpashop/advice/ItemControllerAdvice.java
+++ src/main/java/jpashop/jpashop/advice/ItemControllerAdvice.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice(basePackageClasses = {ItemController.class})
+@RestControllerAdvice(assignableTypes = {ItemController.class})
 public class ItemControllerAdvice {
 
     @ExceptionHandler
