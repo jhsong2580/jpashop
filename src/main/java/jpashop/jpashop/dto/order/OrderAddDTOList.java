cfa25c3608fe1cee98fb4a233f10ff605943bf4d
jhsong2580 <thdwmdgns@naver.com>
2022-09-16 09:00:46 +0900

    fix: 접근 가능한 생성자 추가

--- src/main/java/jpashop/jpashop/dto/order/OrderAddDTOList.java
+++ src/main/java/jpashop/jpashop/dto/order/OrderAddDTOList.java
@@ -1,15 +1,18 @@
 package jpashop.jpashop.dto.order;
 
+import java.util.LinkedList;
 import java.util.List;
 import javax.validation.Valid;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class OrderAddDTOList {
 
     @Valid
-    private List<OrderAddDTO> orders;
+    private List<OrderAddDTO> orders = new LinkedList<>();
 }
