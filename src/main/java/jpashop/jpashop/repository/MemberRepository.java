ae9b00f89c1cbe2df309dcc8c4d655503b23b795
jhsong2580 <thdwmdgns@naver.com>
2022-09-27 20:25:49 +0900

    style

--- src/main/java/jpashop/jpashop/repository/MemberRepository.java
+++ src/main/java/jpashop/jpashop/repository/MemberRepository.java
@@ -5,6 +5,9 @@ import jpashop.jpashop.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
     boolean existsByName(String name);
+
     Optional<Member> findByName(String name);
+
 }
