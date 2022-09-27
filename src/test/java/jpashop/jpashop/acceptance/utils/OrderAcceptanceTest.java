995894855596cc65de60066db3912c8490fde393
jhsong2580 <thdwmdgns@naver.com>
2022-09-27 20:27:55 +0900

    test: order인수테스트 구현

--- src/test/java/jpashop/jpashop/acceptance/utils/OrderAcceptanceTest.java
+++ src/test/java/jpashop/jpashop/acceptance/utils/OrderAcceptanceTest.java
@@ -5,45 +5,54 @@ import static jpashop.jpashop.acceptance.MemberAcceptanceTest.사용자아이디
 import static jpashop.jpashop.acceptance.utils.send.ItemApiUtils.책등록하기;
 import static jpashop.jpashop.acceptance.utils.send.MemberApiUtils.회원가입하기;
 
-import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import jpashop.jpashop.acceptance.AcceptanceTest;
-import jpashop.jpashop.acceptance.MemberAcceptanceTest;
-import jpashop.jpashop.acceptance.utils.assertion.ItemAssertionUtils;
-import jpashop.jpashop.acceptance.utils.send.ItemApiUtils;
-import jpashop.jpashop.acceptance.utils.send.MemberApiUtils;
 import jpashop.jpashop.acceptance.utils.send.OrderApiUtils;
-import jpashop.jpashop.dto.member.form.MemberJoinDTO;
 import jpashop.jpashop.dto.order.OrderAddDTO;
 import jpashop.jpashop.dto.order.OrderAddDTOList;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
 
 public class OrderAcceptanceTest extends AcceptanceTest {
 
-    private String 회원번호="";
-    private String 아이템번호="";
+    private String 회원번호 = "";
+    private String 아이템번호 = "";
 
     @BeforeEach
-    public void init(){
+    public void init() {
         super.init();
-        회원번호 =  회원가입하기(사용자아이디, 사용자비밀번호).body().jsonPath().get("id").toString();
+        회원번호 = 회원가입하기(사용자아이디, 사용자비밀번호).body().jsonPath().get("id").toString();
         아이템번호 = 책등록하기("책", 5000, 50, "작가", "08282").body().jsonPath().get("itemId").toString();
     }
 
 
     @Test
-    public void 주문하기 (){
+    public void 연속주문하기() throws InterruptedException {
 
         OrderAddDTO orderAddDTO = new OrderAddDTO(Long.parseLong(아이템번호), 5);
         OrderAddDTOList orderAddDTOList = new OrderAddDTOList(Arrays.asList(orderAddDTO));
+        List<Integer> results = new LinkedList<>();
+        List<Thread> threads = new LinkedList<>();
 
-        for(int i = 0 ; i< 50; i++){
-            new Thread(() -> OrderApiUtils.주문하기(Long.parseLong(아이템번호), 5))
-                .run();
+        for (int i = 0; i < 70; i++) {
+            Thread thread = new Thread(() -> {
+                ExtractableResponse<Response> 주문하기 = OrderApiUtils.주문하기(Long.parseLong(아이템번호), 5);
+                results.add(주문하기.statusCode());
+            });
+            threads.add(thread);
+            thread.start();
         }
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        Assertions.assertThat(results.stream().filter(integer -> integer == 200).count())
+            .isEqualTo(10);
     }
 }
